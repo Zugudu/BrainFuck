d311f5057b51c68d8d99e3e01d94554326314b42
Zugudu <Zugudu@protonmail.com>
2019-02-26 00:44:48 +0200

    v1.0 add all functional

--- src/q.c
+++ src/q.c
@@ -3,11 +3,30 @@
 unsigned char cell[RAM];
 char ptr=0;
 
+void out(void)
+{
+putc(cell[ptr],stdout);
+}
+
+void outDeb(void)
+{
+printf("%d ",cell[ptr]);
+}
+
 void main(int argc,char *argv[])
 {
 FILE *prg;
 if((prg=fopen(argv[1],"rb"))==NULL)
 	return ;
+void (*trw) (void);
+//INIT
+trw=out;
+//DEFAULT SETTING
+if(argc>2)
+{
+if(*(argv[2])=='d')trw=outDeb;
+}
+//ARG CHECK
 char com;
 while((com=(char)getc(prg))!=EOF)
 {
@@ -32,7 +51,25 @@ while((com=(char)getc(prg))!=EOF)
 	break;
 
 	case '.':
-	putc(cell[ptr],stdout);
+	trw();
+	break;
+
+	case ',':
+	scanf("%d",&cell[ptr]);
+	break;
+
+	case '[':
+	if(cell[ptr]==(char)0){
+	while(getc(prg)!=']'){}
+	}
+	break;
+
+	case ']':
+	if(cell[ptr]!=(char)0){
+	do{
+	fseek(prg,-2,SEEK_CUR);
+	}while((com=getc(prg))!='[');
+	}//IF
 	break;
 	}//SWICTH
 }//WHILE
